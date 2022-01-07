ad1d9177ff940cc170a304f06e6e34bcdc3904ef
Scrirock <Scrirock@gmail.com>
2022-01-07 12:12:52 +0100

    false ajax maker

---
+++ falseAjaxCall.js
@@ -0,0 +1,15 @@
+export default class ajaxMaker {
+
+    url = null
+    xhr = new XMLHttpRequest()
+    method = "GET"
+    oneResponseOk = null
+
+    constructor(url, method, oneResponseOk = null) {
+        this.url = url;
+        this.oneResponseOk = oneResponseOk;
+        if (typeof method === "string") {
+            this.method = method.length in ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'] ? method : 'GET';
+        }
+    }
+}
